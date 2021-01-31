524291f8d5f5feb9f871223500f313c9269a7c58
Joshua Hamilton <joshua@tinydog.io>
2021-01-31 04:12:35 +0000

    Users: Initial table creation and entity mapping.

--- backend/src/entities/user/user.model.ts
+++ backend/src/entities/user/user.model.ts
@@ -1,3 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
 export default class User {
+
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  email!: string;
 
 }
---
+++ backend/src/migrations/1612065701299-CreateUser.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table, TableIndex } from "typeorm";
+
+export class CreateUser1612065701299 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'users',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true
+                },
+                {
+                    name: 'email',
+                    type: 'varchar'
+                },
+            ],
+        }), true);
+
+        await queryRunner.createIndex('users', new TableIndex({
+            name: 'IDX_USERS_EMAIL',
+            columnNames: ['email'],
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropIndex('users', 'IDX_USERS_EMAIL');
+        await queryRunner.dropTable('users');
+    }
+
+}
