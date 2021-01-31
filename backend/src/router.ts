be4c9d79555281df2cf8a2a2d85b80a7a04d3f03
Joshua Hamilton <joshua@tinydog.io>
2021-01-31 02:21:13 +0000

    Remove demo routes.

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -4,14 +4,6 @@ import UserRouter from './entities/user/user.router';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello, world!!!');
-});
-
-router.get('/again', (req, res) => {
-  res.send('Hello, again!!!');
-});
-
 router.use('/customer', CustomerRouter);
 router.use('/user', UserRouter);
 
