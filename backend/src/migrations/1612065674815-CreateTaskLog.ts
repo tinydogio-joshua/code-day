457d645938e484adbc40885ac923593812705f8f
Joshua Hamilton <joshua@tinydog.io>
2021-01-31 04:12:04 +0000

    Task Logs: Initial table creation.

---
+++ backend/src/migrations/1612065674815-CreateTaskLog.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, Table, TableIndex } from "typeorm";
+
+export class CreateTaskLog1612065674815 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'task_logs',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true
+                },
+                {
+                    name: 'duration_minutes',
+                    type: 'int'
+                },
+            ],
+        }), true);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('task_logs');
+    }
+
+}
