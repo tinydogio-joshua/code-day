5414487746f7ae8657d7e2a03fc3d285e7552b7e
Joshua Hamilton <joshua@tinydog.io>
2021-01-31 03:51:54 +0000

    Base customer model.

--- backend/src/entities/customer/customer.model.ts
+++ backend/src/entities/customer/customer.model.ts
@@ -1,3 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('customers')
 export default class Customer {
+
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
 
 }
