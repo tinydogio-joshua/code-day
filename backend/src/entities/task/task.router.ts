c187cae8c3b38a5414da7f36db1c9594e2834ac2
Joshua Hamilton <joshua@tinydog.io>
2021-02-02 18:13:49 +0000

    Add ability to delete a Task.

--- backend/src/entities/task/task.router.ts
+++ backend/src/entities/task/task.router.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-import { createTaskAction, retrieveAllTasksAction, retrieveTaskAction, updateTaskAction } from './actions';
+import {
+  createTaskAction,
+  deleteTaskAction,
+  retrieveAllTasksAction,
+  retrieveTaskAction,
+  updateTaskAction,
+} from './actions';
 
 const router = express.Router();
 
 router.get('/all', retrieveAllTasksAction);
 router.post('/create', createTaskAction);
+router.get('/:id/delete', deleteTaskAction);
 router.post('/:id/update', updateTaskAction);
 router.get('/:id', retrieveTaskAction);
 
