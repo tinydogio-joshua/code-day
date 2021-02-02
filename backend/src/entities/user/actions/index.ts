e95f2dd84cd0c3e9d9ae5c537dbb376cceb0348c
Joshua Hamilton <joshua@tinydog.io>
2021-02-02 23:20:47 +0000

    Add ability to update a user.

--- backend/src/entities/user/actions/index.ts
+++ backend/src/entities/user/actions/index.ts
@@ -2,3 +2,4 @@ export * from './user.create.action';
 export * from './user.delete.action';
 export * from './user.retrieve.action';
 export * from './user.retrieve-all.action';
+export * from './user.update.action';
--- backend/src/entities/user/user.router.ts
+++ backend/src/entities/user/user.router.ts
@@ -1,11 +1,18 @@
 import express from 'express';
-import { createUserAction, deleteUserAction, retrieveAllUsersAction, retrieveUserAction } from './actions';
+import {
+  createUserAction,
+  deleteUserAction,
+  retrieveAllUsersAction,
+  retrieveUserAction,
+  updateUserAction,
+} from './actions';
 
 const router = express.Router();
 
 router.get('/all', retrieveAllUsersAction);
 router.post('/signup', createUserAction);
 router.get('/:id/delete', deleteUserAction);
+router.post('/:id/update', updateUserAction);
 router.get('/:id', retrieveUserAction);
 
 export default router;
