a363b89bb3102bc61308390fd343f6d895ceb627
Joshua Hamilton <joshua@tinydog.io>
2021-02-03 16:58:08 +0000

    Add ability to retrieve users for a company.

--- backend/src/entities/user/actions/index.ts
+++ backend/src/entities/user/actions/index.ts
@@ -2,4 +2,5 @@ export * from './user.create.action';
 export * from './user.delete.action';
 export * from './user.retrieve.action';
 export * from './user.retrieve-all.action';
+export * from './user.retrieve-all-for-company.action';
 export * from './user.update.action';
--- backend/src/entities/user/user.router.ts
+++ backend/src/entities/user/user.router.ts
@@ -3,6 +3,7 @@ import {
   createUserAction,
   deleteUserAction,
   retrieveAllUsersAction,
+  retrieveAllUsersForCompanyAction,
   retrieveUserAction,
   updateUserAction,
 } from './actions';
@@ -10,6 +11,7 @@ import {
 const router = express.Router();
 
 router.get('/all', retrieveAllUsersAction);
+router.get('/all-for-customer/:id', retrieveAllUsersForCompanyAction);
 router.post('/signup', createUserAction);
 router.get('/:id/delete', deleteUserAction);
 router.post('/:id/update', updateUserAction);
