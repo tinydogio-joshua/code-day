390a7e965762457529e79ff1d913ff91710e6658
Joshua Hamilton <joshua@tinydog.io>
2021-02-03 02:21:37 +0000

    Change update customer to use action.

--- backend/src/entities/customer/actions/index.ts
+++ backend/src/entities/customer/actions/index.ts
@@ -2,4 +2,4 @@ export * from './customer.delete.action';
 export * from './customer.register.action';
 export * from './customer.retrieve-all.action';
 export * from './customer.retrieve.action';
-// export * from './customer.update.action';
+export * from './customer.update.action';
--- backend/src/entities/customer/customer.router.ts
+++ backend/src/entities/customer/customer.router.ts
@@ -1,13 +1,18 @@
 import express from 'express';
-import { deleteCustomerAction, registerCustomerAction, retrieveAllCustomersAction, retrieveCustomerAction } from './actions';
-import Controller from './customer.controller';
+import {
+  deleteCustomerAction,
+  registerCustomerAction,
+  retrieveAllCustomersAction,
+  retrieveCustomerAction,
+  updateCustomerAction,
+} from './actions';
 
 const router = express.Router();
 
 router.get('/all', retrieveAllCustomersAction);
 router.post('/register', registerCustomerAction);
 router.get('/:id/delete', deleteCustomerAction);
-router.post('/:id/update', Controller.update);
+router.post('/:id/update', updateCustomerAction);
 router.get('/:id', retrieveCustomerAction);
 
 export default router;
